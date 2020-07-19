53423831a92a6e365f3ac081ada7f416176562b3
Muhammad Gunawan <mubarokg@gmail.com>
2020-07-19 11:28:15 +0700

    project gunawan

---
+++ routes/api/users.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//@route POST api/users
+// @desc test route
+// @access public atau private
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+
+    res.send('User Route');
+});
+
+module.exports = router;
