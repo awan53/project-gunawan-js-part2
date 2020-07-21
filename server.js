905457e67a9ca8d8e237911ede522d0414841762
awan53 <35141512+awan53@users.noreply.github.com>
2020-07-21 10:44:25 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
+//memanggil connectDB
+connectDB();
+//initianl Middleware
+app.use(express.json({ extended: false }));
+
+//test route index
 app.get('/', (req, res) => res.json('APIs Works!'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/post', require('./routes/api/post'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running in PORT ${PORT}`));
---
+++ users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+// @route POST api/users
+// @desc Register user
+// @access Public
+router.post('/', (req, res) => {
+// test request
+console.log(req.body);
+res.send('User route')
+});
+// export route
+module.exports = router;
